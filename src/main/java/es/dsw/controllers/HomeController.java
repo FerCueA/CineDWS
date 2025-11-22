801ee211d5e80c1c9d0404cf11ea3a1d01a57bcc
FerCueA <fercuea@protonmail.com>
2025-11-22 10:30:13 +0000

    actualizacion cerrarSession

---
+++ src/main/java/es/dsw/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package es.dsw.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.support.SessionStatus;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home(SessionStatus status, HttpSession session) {
+        status.setComplete();
+        session.invalidate();
+        return "redirect:/index";
+    }
+}
